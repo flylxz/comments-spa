ec7ad33b8dcac7c57ade2e5b8d583dc8cf328b99
Alexandr Gladchenko <front.end.aam.dzencode@gmail.com>
2026-06-19 18:40:53 +0300

    fix: remove pre-deploy command from render.yaml and add it to start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,8 @@ export PORT="${PORT:-10000}"
 
 envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
+bunx prisma migrate deploy
+
 PORT=3000 bun dist/app.js &
 
 exec nginx -g 'daemon off;'
